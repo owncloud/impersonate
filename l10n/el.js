171cb55371afba5403378fa0e7c61528c5390e03
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-05-24 01:08:02 +0200

    [tx-robot] updated from transifex

---
+++ l10n/el.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Impersonate" : "Εκπροσωπώ",
+    "Error" : "Σφάλμα",
+    "Logged in as {currentUser}" : "Συνδεθήκατε ως {τρέχων Χρήστης }",
+    "Impersonate Settings" : "Προσωποποίηση  ρυθμίσεων",
+    "Allow group admins to impersonate users from these groups" : " Επιτρέπεται στους διαχειριστές ομάδων να εκπροσωπούν τους χρήστες από αυτές τις ομάδες"
+},
+"nplurals=2; plural=(n != 1);");
