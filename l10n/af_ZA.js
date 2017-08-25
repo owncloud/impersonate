9aca639b44a087ad598c9fe9f888f63fcda9c26a
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-25 01:29:25 +0000

    [tx-robot] updated from transifex

---
+++ l10n/af_ZA.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Impersonate" : "Naboots",
+    "Error" : "Fout",
+    "Logged in as {currentUser}" : "Aangeteken as {currentUser}",
+    "Impersonate Settings" : "Naboots-instellings",
+    "Allow group admins to impersonate users from these groups" : "Laat groepadmins toe om gebruikers van hierdie groep na te boots"
+},
+"nplurals=2; plural=(n != 1);");
