9f3afcc0bfb4cf6a0f52abea128cbf4434c8e2ac
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-26 01:29:26 +0000

    [tx-robot] updated from transifex

---
+++ l10n/nb_NO.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Impersonate" : "Gjør upersonlig",
+    "Error" : "Feil",
+    "Logged in as {currentUser}" : "Innlogget som {currentUser}",
+    "Impersonate Settings" : "Innstillinger for upersonlighet",
+    "Allow group admins to impersonate users from these groups" : "Tillat gruppe administrator å gjøre personer i disse gruppene upersonlig"
+},
+"nplurals=2; plural=(n != 1);");
