7d428928e883f1a3cbb0c4acf314fb138867dc41
ownCloud Jenkins <jenkins@owncloud.com>
2017-10-16 01:29:16 +0000

    [tx-robot] updated from transifex

--- l10n/af_ZA.js
+++ l10n/af_ZA.js
@@ -1,6 +1,8 @@
 OC.L10N.register(
     "impersonate",
     {
+    "Can not impersonate" : "Kan nie naboots nie",
+    "Unexpected error occured" : "Onverwagte fout het voorgekom",
     "Impersonate" : "Naboots",
     "Error" : "Fout",
     "Logged in as {currentUser}" : "Aangeteken as {currentUser}",
