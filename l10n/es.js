a677d5811643b47cc85504dacd73a2d984b341e9
ownCloud Jenkins <jenkins@owncloud.com>
2017-10-18 01:29:14 +0000

    [tx-robot] updated from transifex

--- l10n/es.js
+++ l10n/es.js
@@ -1,6 +1,8 @@
 OC.L10N.register(
     "impersonate",
     {
+    "Can not impersonate" : "No se puede suplantar",
+    "Unexpected error occured" : "Error inesperado.",
     "Impersonate" : "Suplantar",
     "Error" : "Error",
     "Logged in as {currentUser}" : "Conectado como {currentUser}",
