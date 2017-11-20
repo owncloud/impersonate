f14a0e07c00524db0d298b5727e165ee37f7abbf
ownCloud Jenkins <jenkins@owncloud.com>
2017-11-20 01:29:21 +0000

    [tx-robot] updated from transifex

---
+++ l10n/nn_NO.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate" : "Kan ikkje etterlikne",
+    "Unexpected error occured" : "Ein uventa feil oppstod",
+    "Impersonate" : "Etterlikn",
+    "Error" : "Feil",
+    "Logged in as {currentUser}" : "Logga på som {currentUser}",
+    "Impersonate Settings" : "Innstilling for etterlikning",
+    "Allow group admins to impersonate users from these groups" : "Tillat administrator for gruppe å etterlikne brukarar frå desse gruppene"
+},
+"nplurals=2; plural=(n != 1);");
