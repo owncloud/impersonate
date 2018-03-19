0a0625ce247dba672864a86fab7c8082da16ea83
Julian1998 <julimueller1998@gmail.com>
2018-03-19 11:52:27 +0100

    Translation fix

--- js/impersonate_logout.js
+++ js/impersonate_logout.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 	var context = {
 		docUrl: OC.generateUrl('apps/files'),
-		displayText: t('core','Logged in as {currentUser}', {'currentUser': OC.getCurrentUser().uid})
+		displayText: t('impersonate','Logged in as {currentUser}', {'currentUser': OC.getCurrentUser().uid})
 	};
 
 	var $html = OCA.Impersonate['impersonateNotification'](context);
