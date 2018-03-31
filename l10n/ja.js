507a52c69666f170135a127a9befb6105858705b
ownCloud Jenkins <jenkins@owncloud.com>
2018-03-31 01:29:29 +0000

    [tx-robot] updated from transifex

---
+++ l10n/ja.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate" : "コピーすることは出来ません",
+    "Unexpected error occured" : "予期しないエラーが発生しました",
+    "Impersonate" : "コピー",
+    "Error" : "エラー",
+    "Logged in as {currentUser}" : " {currentUser}としてログインしました",
+    "Impersonate Settings" : "コピー設定",
+    "Allow group admins to impersonate users from these groups" : "グループ管理者がこれらのグループからユーザーをコピーするのを許可する"
+},
+"nplurals=1; plural=0;");
