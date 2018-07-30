a03824caf515798fef1438e33c35d87e3f67de38
ownCloud Jenkins <jenkins@owncloud.com>
2018-07-30 01:29:17 +0000

    [tx-robot] updated from transifex

---
+++ l10n/mk.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate" : "Неможам да имитирам",
+    "Unexpected error occured" : "Се случи неочекувана грешка",
+    "Impersonate" : "Имитирање",
+    "Error" : "Грешка",
+    "Logged in as {currentUser}" : "Најавен како {currentUser}",
+    "Impersonate Settings" : "Подесување за имитирање",
+    "Allow group admins to impersonate users from these groups" : "Дозволи администраторите на групите да ги имитираат корисниците од овие групи"
+},
+"nplurals=2; plural=(n % 10 == 1 && n % 100 != 11) ? 0 : 1;");
