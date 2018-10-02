d1e113e47a5c4c39be897ed1332a1aea6c7fe54a
ownClouders <devops@owncloud.com>
2018-10-02 05:09:44 +0000

    [tx] updated from transifex

---
+++ l10n/en_GB.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate" : "Can not impersonate",
+    "Unexpected error occured" : "Unexpected error occured",
+    "Impersonate" : "Impersonate",
+    "Error" : "Error",
+    "Logged in as {currentUser}" : "Logged in as {currentUser}",
+    "Impersonate Settings" : "Impersonate Settings",
+    "Allow group admins to impersonate users from these groups" : "Allow group admins to impersonate users from these groups"
+},
+"nplurals=2; plural=(n != 1);");
