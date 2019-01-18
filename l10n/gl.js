e150214071434ed67503db447e6eedd4536cb25e
ownClouders <devops@owncloud.com>
2019-01-18 05:33:51 +0000

    [tx] updated from transifex

---
+++ l10n/gl.js
@@ -0,0 +1,12 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate" : "Non foi posíbel suplantar ao usuario",
+    "Unexpected error occured" : "Produciuse un erro non agardado",
+    "Impersonate" : "Suplantar",
+    "Error" : "Erro",
+    "Logged in as {currentUser}" : "Accedeu como {currentUser}",
+    "Impersonate Settings" : "Axustes da suplantación",
+    "Allow group admins to impersonate users from these groups" : "Permitir que os administradores do grupo substitúan aos usuarios destes grupos"
+},
+"nplurals=2; plural=(n != 1);");
