0acd8adafcacf7a2993170e2aeb976eefe0c5f28
ownClouders <devops@owncloud.com>
2019-02-14 05:00:52 +0000

    [tx] updated from transifex

--- l10n/ru.js
+++ l10n/ru.js
@@ -6,6 +6,8 @@ OC.L10N.register(
     "Impersonate" : "Притвориться",
     "Error" : "Ошибка",
     "Logged in as {currentUser}" : "Текущий вход под {currentUser}",
-    "Impersonate Settings" : "Притвориться — настройки"
+    "Impersonate Settings" : "Притвориться — настройки",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "Разрешить каждому администратору группы притворяться любым членом той группы, администратором которой он является",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "Разрешить администраторам заданных групп притворяться пользователями из этих групп"
 },
 "nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
