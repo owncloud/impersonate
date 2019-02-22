6b7f895d662394977646a939dab87a6fa12fca64
ownClouders <devops@owncloud.com>
2019-02-22 05:00:57 +0000

    [tx] updated from transifex

--- l10n/th_TH.js
+++ l10n/th_TH.js
@@ -6,6 +6,8 @@ OC.L10N.register(
     "Impersonate" : "ปลอมตัว",
     "Error" : "ข้อผิดพลาด",
     "Logged in as {currentUser}" : "ลงชื่อเข้าใช้ด้วย {currentUser}",
-    "Impersonate Settings" : "ตั้งค่าการปลอมเป็นบุคคลอื่น"
+    "Impersonate Settings" : "ตั้งค่าการปลอมเป็นบุคคลอื่น",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "อนุญาตให้ผู้ดูแลระบบทั้งหมดของกลุ่มสามารถปลอมตัวเป็นผู้ใช้ภายในกลุ่มที่พวกเขาดูแลอยู่",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "อนุญาตให้ผู้ดูแลระบบบางกลุ่มสามารถปลอมตัวเป็นผู้ใช้ภายในกลุ่มที่พวกเขาดูแลอยู่"
 },
 "nplurals=1; plural=0;");
