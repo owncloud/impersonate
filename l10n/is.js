e789ed0978793d721b187ddec5aacbaae5e22647
ownClouders <devops@owncloud.com>
2019-04-12 05:01:05 +0000

    [tx] updated from transifex

--- l10n/is.js
+++ l10n/is.js
@@ -6,6 +6,8 @@ OC.L10N.register(
     "Impersonate" : "Taka yfir notanda",
     "Error" : "Villa",
     "Logged in as {currentUser}" : "Skráður inn sem {currentUser}",
-    "Impersonate Settings" : "Taka yfir stillingar notanda"
+    "Impersonate Settings" : "Taka yfir stillingar notanda",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "Gerir hópstjórnendum kleift að taka yfir notendur úr þessum hópum",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "Gerir hópstjórnendum kleift að taka yfir notendur úr tilteknum hópum"
 },
 "nplurals=2; plural=(n % 10 != 1 || n % 100 == 11);");
