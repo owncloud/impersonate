83bba8e29ae1eef382e30797e6600faa7dfb93a3
ownClouders <devops@owncloud.com>
2019-05-21 05:01:23 +0000

    [tx] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -6,6 +6,8 @@ OC.L10N.register(
     "Impersonate" : "להתחזות",
     "Error" : "שגיאה",
     "Logged in as {currentUser}" : "נכנסת בתור {currentUser}",
-    "Impersonate Settings" : "הגדרות התחזות"
+    "Impersonate Settings" : "הגדרות התחזות",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "מאפשר לכל מנהלי הקבוצות להתחזות לכל משתמש בקבוצה אותם הם מנהלים",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "מאפשר למנהלי קבוצות מסויימות להתחזות לכל משתמש בקבוצה אותם הם מנהלים"
 },
 "nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
