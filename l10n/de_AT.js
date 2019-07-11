76a7516637d6cc468676a9d62c96f89cf62176c8
ownClouders <devops@owncloud.com>
2019-07-11 05:01:34 +0000

    [tx] updated from transifex

---
+++ l10n/de_AT.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate" : "Sich ausgeben als dieser Nutzer nicht möglich.",
+    "Unexpected error occured" : "Es  ist ein unerwarteter Fehler aufgetreten.",
+    "Impersonate" : "Sich ausgeben als",
+    "Error" : "Fehler",
+    "Logged in as {currentUser}" : "Eingeloggt als {aktuellerNutzer}",
+    "Impersonate Settings" : "Einstellungen für das Imitieren anderer Nutzer.",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "Allen Gruppen Administratoren erlauben sich als Nutzer jener Gruppe auszugeben.",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "Gruppen Administratoren einer konkreten Gruppe erlauben sich als Nutzer jener Gruppe auszugeben."
+},
+"nplurals=2; plural=(n != 1);");
