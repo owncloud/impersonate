b168258241cfeaa86305f819254139861ed4b3a4
ownClouders <devops@owncloud.com>
2019-09-04 01:06:28 +0000

    [tx] updated from transifex

--- l10n/eu.js
+++ l10n/eu.js
@@ -7,6 +7,7 @@ OC.L10N.register(
     "Error" : "Errorea",
     "Logged in as {currentUser}" : "Saioa hasita {currentUser} bezala",
     "Impersonate Settings" : "Itxura egitearen Ezarpenak",
-    "Allow all group admins to impersonate users within the groups they are admins of" : "Talde administratzaile guztiak baimendu administratzen dituzten taldekideen itxura egitera"
+    "Allow all group admins to impersonate users within the groups they are admins of" : "Talde administratzaile guztiak baimendu administratzen dituzten taldekideen itxura egitera",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "Talde jakin batzuetako administratzaileak baimendu bertako taldekideen itxura egiteko"
 },
 "nplurals=2; plural=(n != 1);");
