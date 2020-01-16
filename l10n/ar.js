5a233a6fe3faca347658748d92f3e5c6b208eb86
Dominik Schmidt <domme@tomahawk-player.org>
2020-01-16 02:25:52 +0000

    [tx] updated from transifex

---
+++ l10n/ar.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate" : "تعذّر التمثيل",
+    "Unexpected error occured" : "حدث خطأ غير متوقع",
+    "Impersonate" : "تمثيل",
+    "Error" : "خطأ",
+    "Logged in as {currentUser}" : "تم تسجيل الدخول بصفة {currentUser}",
+    "Impersonate Settings" : "إعدادات التمثيل",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "السماح لجميع مسؤولي المجموعة بتمثيل المستخدمين في المجموعات التي هم مسؤولون فيها",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "السماح لمسؤولي بعض المجموعات المحددة بتمثيل المستخدمين ضمن هذه المجموعات"
+},
+"nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5;");
