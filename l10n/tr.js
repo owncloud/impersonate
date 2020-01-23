9861d91de762ab9cb1a50071260d6967fc9ae40e
Dominik Schmidt <domme@tomahawk-player.org>
2020-01-23 02:27:33 +0000

    [tx] updated from transifex

--- l10n/tr.js
+++ l10n/tr.js
@@ -1,10 +1,13 @@
 OC.L10N.register(
     "impersonate",
     {
+    "Can not impersonate" : "Taklit edilemiyor",
+    "Unexpected error occured" : "Beklenmeyen hata oluştu",
     "Impersonate" : "Taklit etme",
     "Error" : "Hata",
     "Logged in as {currentUser}" : "{currentUser} olarak giriş yapıldı.",
     "Impersonate Settings" : "Taklit etme seçenekleri",
-    "Allow group admins to impersonate users from these groups" : "Grup yöneticilerinin, üyeleri taklit edebilmelerine izin ver."
+    "Allow all group admins to impersonate users within the groups they are admins of" : "Tüm grup yöneticilerinin, yönetici oldukları gruplardaki kullanıcıları taklit etmelerine izin ver",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "Belirli grupların grup yöneticilerinin bu gruplardaki kullanıcıları taklit etmelerine izin ver"
 },
 "nplurals=2; plural=(n > 1);");
