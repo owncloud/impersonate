7826a84686a709ee90c4eb9160ce3e349db83db3
Dominik Schmidt <domme@tomahawk-player.org>
2020-02-09 02:27:21 +0000

    [tx] updated from transifex

---
+++ l10n/bg_BG.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate" : "Не може да се представи",
+    "Unexpected error occured" : "Възникна неочаквана грешка",
+    "Impersonate" : "Представяне",
+    "Error" : "Грешка",
+    "Logged in as {currentUser}" : "Влезли сте като {currentUser}",
+    "Impersonate Settings" : "Представяне Настройки",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "Разрешете на всички администратори на групи да се представят и за потребители в рамките на групите, на които са администратори",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "Разрешаване на администратори на конкретни групи, за да се представят и за потребителите в тези групи"
+},
+"nplurals=2; plural=(n != 1);");
