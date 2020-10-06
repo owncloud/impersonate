082884ab02495bf490e51bcaadd5bc37f6e9286d
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-06 01:24:55 +0000

    [tx] updated from transifex

---
+++ l10n/zh_TW.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate" : "無法假冒",
+    "Unexpected error occured" : "發生意外錯誤",
+    "Impersonate" : "模仿",
+    "Error" : "錯誤",
+    "Logged in as {currentUser}" : "以{currentUser}身份登錄",
+    "Impersonate Settings" : "模擬設定"
+},
+"nplurals=1; plural=0;");
