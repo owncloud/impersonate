d5d84e72e61d34903739fd3be5e7b853c199db28
ownClouders <devops@owncloud.com>
2022-05-24 00:36:10 +0000

    [tx] updated from transifex

--- l10n/pt_BR.js
+++ l10n/pt_BR.js
@@ -10,4 +10,4 @@ OC.L10N.register(
     "Allow all group admins to impersonate users within the groups they are admins of" : "Permitir que todos os administradores do grupo representem usuários nos grupos em que são administradores",
     "Allow group admins of specific groups to impersonate the users within those groups" : "Permitir que administradores de grupo de grupos específicos personifiquem os usuários nesses grupos"
 },
-"nplurals=2; plural=(n > 1);");
+"nplurals=3; plural=(n == 0 || n == 1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
