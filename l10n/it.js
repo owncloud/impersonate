aab88d11ba9f4fb4976390e2ee59e7e41a2949e9
ownClouders <devops@owncloud.com>
2022-07-16 00:06:04 +0000

    [tx] updated from transifex

--- l10n/it.js
+++ l10n/it.js
@@ -6,6 +6,8 @@ OC.L10N.register(
     "Impersonate" : "Cambia utente",
     "Error" : "Errore",
     "Logged in as {currentUser}" : "Sei loggato come {currentUser}",
-    "Impersonate Settings" : "Impostazioni impersonalizzazione"
+    "Impersonate Settings" : "Impostazioni impersonalizzazione",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "Consenti agli amministratori di gruppo di impersonare utenti dei gruppi di cui sono amministratori",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "Consenti agli amministratori di gruppi specifici di impersonare gli utenti degli stessi gruppi."
 },
 "nplurals=3; plural=n == 1 ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
