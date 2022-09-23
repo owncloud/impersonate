643b91dc61791c3c47215415ead914cddf3eb0d7
ownClouders <devops@owncloud.com>
2022-09-23 01:19:37 +0000

    [tx] updated from transifex

--- l10n/sq.js
+++ l10n/sq.js
@@ -3,12 +3,13 @@ OC.L10N.register(
     {
     "Can not impersonate. Please contact your server administrator to allow impersonation." : "S’imitohet dot. Që të lejohen imitime, ju lutemi, lidhuni me përgjegjësin e shërbyesit tuaj.",
     "Can not impersonate" : "S’imitohet dot",
-    "Unexpected error occured" : "Ndodhi një gabim i papritur",
-    "Unexpected error occured." : "Ndodhi gabim i papritur.",
+    "Unexpected error occurred" : "Ndodhi gabim i papritur",
+    "Unexpected error occurred." : "Ndodhi gabim i papritur.",
     "Impersonate" : "Imitoje",
     "Error" : "Gabim",
     "Logged in as {currentUser}" : "I futur si {currentUser}",
     "Impersonate Settings" : "Rregullime Imitimesh",
+    "Only an administrator is allowed to impersonate users" : "Vetëm përgjegjësve u lejohet të shtiren si përdorues ",
     "Allow all group admins to impersonate users within the groups they are admins of" : "Lejo krejt përgjegjësit e grupeve të imitojnë përdorues prej grupesh për të cilët janë përgjegjës",
     "Allow group admins of specific groups to impersonate the users within those groups" : "Lejo përgjegjës grupesh për grupe të caktuar të imitojnë përdoruesit brenda këtyre grupeve"
 },
