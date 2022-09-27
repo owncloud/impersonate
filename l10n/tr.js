ab4d26c8c126371cf4435ad0f37054ec5520000a
ownClouders <devops@owncloud.com>
2022-09-27 00:24:13 +0000

    [tx] updated from transifex

--- l10n/tr.js
+++ l10n/tr.js
@@ -3,12 +3,13 @@ OC.L10N.register(
     {
     "Can not impersonate. Please contact your server administrator to allow impersonation." : "Kimliğe bürünülemez. Kimliğe bürünmeye izin vermek için lütfen sunucu yöneticinizle iletişime geçin.",
     "Can not impersonate" : "Kimliğe bürünülemiyor",
-    "Unexpected error occured" : "Beklenmeyen hata oluştu",
-    "Unexpected error occured." : "Beklenmeyen hata oluştu.",
+    "Unexpected error occurred" : "Beklenmedik hata oluştu",
+    "Unexpected error occurred." : "Beklenmedik hata oluştu.",
     "Impersonate" : "Kimliğe Bürünme",
     "Error" : "Hata",
     "Logged in as {currentUser}" : "{currentUser} olarak giriş yapıldı.",
     "Impersonate Settings" : "Kimliğe bürünme seçenekleri",
+    "Only an administrator is allowed to impersonate users" : "Yalnızca bir yöneticinin kullanıcıların kimliğine bürünmesine izin verilir",
     "Allow all group admins to impersonate users within the groups they are admins of" : "Tüm grup yöneticilerinin, yönetici oldukları gruplardaki kullanıcıların kimliklerine bürünmelerine izin ver",
     "Allow group admins of specific groups to impersonate the users within those groups" : "Belirli grupların grup yöneticilerinin bu gruplardaki kullanıcıların kimliklerine bürünmelerine izin ver"
 },
