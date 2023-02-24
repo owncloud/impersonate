9022c6651c98e750450c3a056537719dbd3fc7e1
ownClouders <devops@owncloud.com>
2023-02-24 00:42:28 +0000

    [tx] updated from transifex

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -3,12 +3,13 @@ OC.L10N.register(
     {
     "Can not impersonate. Please contact your server administrator to allow impersonation." : "Kann den Benutzer nicht imitieren. Bitte kontaktieren sie den Server Administrator um die Imitation zu erlauben.",
     "Can not impersonate" : "Benutzer Imitation fehlgeschlagen",
-    "Unexpected error occured" : "Ein unerwarteter Fehler ist aufgetreten",
-    "Unexpected error occured." : "Ein unerwarteter Fehler ist aufgetreten",
+    "Unexpected error occurred" : "Ein unerwarteter Fehler ist aufgetreten",
+    "Unexpected error occurred." : "Ein unerwarteter Fehler ist aufgetreten.",
     "Impersonate" : "Als imitierter Benutzer ausgeben",
     "Error" : "Fehler",
     "Logged in as {currentUser}" : "Eingeloggt als {currentUser}",
     "Impersonate Settings" : "Benutzer Imitierungs-Einstellungen",
+    "Only an administrator is allowed to impersonate users" : "Nur der Administrator darf sich als andere Benutzer ausgeben.",
     "Allow all group admins to impersonate users within the groups they are admins of" : "Erlauben Sie allen Gruppenadministratoren, die Rolle von Benutzern innerhalb der Gruppen anzunehmen (imitieren), für die sie Administratoren sind.",
     "Allow group admins of specific groups to impersonate the users within those groups" : "Erlauben Sie Gruppenadministratoren bestimmter Gruppen, die Rolle von Benutzern dieser Gruppe anzunehmen (imitieren)."
 },
