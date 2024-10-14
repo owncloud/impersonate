331e2e7ad84df8a654d084d0cee46090aec5930c
ownClouders <devops@owncloud.com>
2024-10-14 01:39:01 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,15 @@
+OC.L10N.register(
+    "impersonate",
+    {
+    "Can not impersonate. Please contact your server administrator to allow impersonation." : "تەقلىد قىلالمايدۇ. تەقلىد قىلىشقا رۇخسەت قىلىش ئۈچۈن مۇلازىمېتىر باشقۇرغۇچىڭىز بىلەن ئالاقىلىشىڭ.",
+    "Can not impersonate" : "تەقلىد قىلالمايدۇ",
+    "Unexpected error occurred" : "ئويلىمىغان خاتالىق يۈز بەردى",
+    "Impersonate" : "تەقلىد قىلىش",
+    "Error" : "خاتالىق",
+    "Logged in as {currentUser}" : "{CurrentUser as دەپ تىزىملىتىڭ",
+    "Impersonate Settings" : "تەڭشەكلەر",
+    "Only an administrator is allowed to impersonate users" : "پەقەت باشقۇرغۇچىنىڭلا ئابونتلارنى دوراشقا رۇخسەت قىلىنىدۇ",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "بارلىق گۇرۇپپا باشقۇرغۇچىلارنىڭ ئۆزلىرى باشقۇرۇۋاتقان گۇرۇپپىلار ئىچىدىكى ئابونتلارنى تەقلىد قىلىشىغا يول قويۇڭ",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "ئالاھىدە گۇرۇپپىلارنىڭ گۇرۇپپا باشقۇرغۇچىلىرىنىڭ شۇ گۇرۇپپىلار ئىچىدىكى ئىشلەتكۈچىلەرگە تەقلىد قىلىشىغا يول قويۇڭ"
+},
+"nplurals=2; plural=(n != 1);");
