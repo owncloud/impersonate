e17aec28f439bcaa77d304730f7dbfbbf8749259
ownClouders <devops@owncloud.com>
2025-10-31 00:05:13 +0000

    [tx] updated from transifex

--- l10n/nl.js
+++ l10n/nl.js
@@ -1,11 +1,15 @@
 OC.L10N.register(
     "impersonate",
     {
-    "Unexpected error occured" : "Onverwachte fout opgetreden",
+    "Can not impersonate. Please contact your server administrator to allow impersonation." : "impersonificatie niet mogelijk. Neem contact op met uw serverbeheerder om impersonificatie toe te staan.",
+    "Can not impersonate" : "Impersonificatie niet mogelijk",
+    "Unexpected error occurred" : "Er is een onverwachte fout opgetreden",
     "Impersonate" : "Imiteren",
     "Error" : "Fout",
     "Logged in as {currentUser}" : "Ingelogd als {currentUser}",
     "Impersonate Settings" : "Imiteerinstellingen",
-    "Allow group admins to impersonate users from these groups" : "Toestaan dat groepsbeheerders gebruikers uit hun groepen imiteren"
+    "Only an administrator is allowed to impersonate users" : "Alleen een beheerder mag gebruikers impersonificeren",
+    "Allow all group admins to impersonate users within the groups they are admins of" : "Alle groepsbeheerders toestaan gebruikers te impersonificeren binnen de groepen waar ze beheerder van zijn",
+    "Allow group admins of specific groups to impersonate the users within those groups" : "Groepsbeheerders van specifieke groepen toestaan de gebruikers binnen die groepen te impersonificeren"
 },
 "nplurals=2; plural=(n != 1);");
